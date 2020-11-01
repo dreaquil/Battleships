f2a9bd757d640db14da1ab8fbf8c4e2ee705cae0
david.mccabe <david.mccabe082@gmail.com>
2020-11-01 12:17:51 +0000

    completed provisional database schema
    inspired by SQL-Battleship-2008 schema diagram here: https://www.codeproject.com/Articles/22147/SQL-Battleship-2008

--- sql/001_init.sql
+++ sql/001_init.sql
@@ -1,34 +1,56 @@
-CREATE TABLE ships
-(
-    id            INTEGER PRIMARY KEY,
-    playerId      INTEGER,
-    name          TEXT,
-    symbol        TEXT,
-    coordinates   TEXT
-);
-
-INSERT INTO ships VALUES (1,  1, "Aircraft Carrier", "A", "A1,A2,A3,A4,A5");
-INSERT INTO ships VALUES (2,  2, "Aircraft Carrier", "A", "A1,A2,A3,A4,A5");
-INSERT INTO ships VALUES (3,  1, "Battleship", "B", "B1,B2,B3,B4");
-INSERT INTO ships VALUES (4,  2, "Battleship", "B", "B1,B2,B3,B4");
-INSERT INTO ships VALUES (5,  1, "Cruiser", "C", "C1,C2,C3");
-INSERT INTO ships VALUES (6,  2, "Cruiser", "C", "C1,C2,C3");
-INSERT INTO ships VALUES (7,  1, "Submarine", "S", "D3,D4,D5");
-INSERT INTO ships VALUES (8,  2, "Submarine", "S", "D3,D4,D5");
-INSERT INTO ships VALUES (9,  1, "Destroyer", "D", "D1,D2");
-INSERT INTO ships VALUES (10, 2, "Destroyer", "D", "D1,D2");
-
---SELECT * FROM ships;
-
-
-
 CREATE TABLE games (
     id          INTEGER PRIMARY KEY,
     status      VARCHAR
 );
 
-INSERT INTO games
-(status) VALUES ('WAITING_FOR_PLAYER_ONE');
+INSERT INTO games (status) VALUES ('WAITING_FOR_PLAYER_ONE');
+
+
+CREATE TABLE players (
+    id       INTEGER PRIMARY KEY,
+    name     VARCHAR UNIQUE
+);
+
+CREATE TABLE ships (
+    id            INTEGER PRIMARY KEY,
+    game_id       VARCHAR,
+    player_id     VARCHAR,
+    ship_id       VARCHAR,
+    coordinates   VARCHAR
+);
+
+CREATE TABLE ship_classes
+(
+    id            INTEGER PRIMARY KEY,
+    name          VARCHAR,
+    symbol        VARCHAR,
+    length        VARCHAR
+);
+
+INSERT INTO ship_classes (name, symbol, length) VALUES ("Aircraft Carrier", "A", 5);
+INSERT INTO ship_classes (name, symbol, length) VALUES ("Battleship", "B", 4);
+INSERT INTO ship_classes (name, symbol, length) VALUES ("Cruiser", "C", 3);
+INSERT INTO ship_classes (name, symbol, length) VALUES ("Submarine", "S", 3);
+INSERT INTO ship_classes (name, symbol, length) VALUES ("Destroyer", "D", 2);
+
+
+CREATE TABLE pegs
+(
+    id            INTEGER PRIMARY KEY,
+    game_id       VARCHAR,
+    player_id     VARCHAR,
+    coordinate    VARCHAR,
+    colour        VARCHAR
+);
+
+
+
+
+
+
+
+
+
 
 CREATE TABLE AppUser (
     id          INTEGER PRIMARY KEY,
@@ -41,3 +63,15 @@ CREATE TABLE AppUser (
 INSERT INTO AppUser
 (username, email, password, role) VALUES ('admin', 'admin@domain.com', 'admin', 'ROLE_ADMIN');
 
+CREATE TABLE ships
+(
+    id            INTEGER PRIMARY KEY,
+    game_id       INTEGER,
+    player_id     INTEGER,
+    name          VARCHAR,
+    symbol        VARCHAR,
+    coordinates   VARCHAR
+);
+
+
+--SELECT * FROM ships;
